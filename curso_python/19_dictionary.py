744e0fefd2445c53ae0927fbdaca8c2a134e626b
Esteban-Duarte73 <eduart73.eadp@gmail.com>
2023-05-08 15:36:58 -0500

    [update] addd dictionary

---
+++ curso_python/19_dictionary.py
@@ -0,0 +1,16 @@
+#otra estructura son los diccionarios  
+my_dict = {}
+print(type(my_dict))
+
+my_dict = {
+    'name' : 'Esteban',
+    'avion' : 'bla bla bla',
+    'last_name' : 'Duarte',
+    'age' : 33
+}
+
+#vemos la cantidad de valores del diccionario con len
+print(my_dict)
+print(len(my_dict))
+#en este caso ser 4: name, avion, apellido, edad
+
